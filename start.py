a4e719132b61d0b7ca71449392e44b416a77a8e4
Kotaz <semechkagent@gmail.com>
2022-06-25 14:01:56 +0300

    Fix argument processing

--- start.py
+++ start.py
@@ -1,15 +1,23 @@
 import sys
-
+import os
 import whs
 
 def read(filename):
-	with open(f'{filename}.whc', 'r') as file:
+	with open(f'{filename}', 'r') as file:
 		for n, line in enumerate(file, 1):
 			line = line.rstrip('\n')
 			try:
 				whs.Core.run(line)
 			except IndexError:
 				print('Argument is not found')
-			
-read(sys.argv[1])
+args = sys.argv
+if len(args) != 2:
+	print('Использование: python start.py <файл>')
+else:
+	if not args[1].endswith('.whc'):
+		args[1] += '.whc'
+	if os.path.exists(args[1]):
+		read(args[1])
+	else:
+		print(f'Файл {args[1]} не существует')
 
